110547c06367cff6717de7253d30f4c840b6651b
Riken <leijian_jian@126.com>
2016-03-20 15:08:40 +0800

    首次嵌入项目文件

---
+++ MyMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMVC5.Startup))]
+namespace MyMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
